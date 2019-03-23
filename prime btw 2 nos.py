5469cfef35daeb76919161c1114607405f5d1985
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-03-23 10:22:24 +0530

    Add files via upload

--- prime btw 2 nos.py
+++ prime btw 2 nos.py
@@ -3,7 +3,7 @@ class pri:
         self.a=[int(i) for i in input().split()]
         self.b=[]
     def fin(self):
-        for i in range(self.a[0]+1,self.a[1]-1):
+        for i in range(self.a[0]+1,self.a[1]):
             if(i>1):
                 for j in range(2,i):
                     if(i%j==0):
