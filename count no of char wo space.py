a200c503fe00b7b28adb9146857e901c87767957
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-03-26 19:35:21 +0530

    Add files via upload

---
+++ count no of char wo space.py
@@ -0,0 +1,11 @@
+class ch:
+    def __init__(self):
+        self.s=str(input())
+        l=len(self.s)
+        c=0
+        for i in range(0,l-1):
+            if(self.s[i]==' '):
+                c=c+1
+        l-=c
+        print(l)
+o=ch()
