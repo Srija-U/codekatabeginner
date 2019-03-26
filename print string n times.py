dd71be227bcac0ea61785196353f24bb43624d56
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-03-27 05:24:30 +0530

    Add files via upload

---
+++ print string n times.py
@@ -0,0 +1,8 @@
+class nt:
+    def __init__(self):
+        self.lis=[str(i) for i in input().split()]
+        n=int(self.lis[1])
+        for i in range(0,n):
+            print(self.lis[0])
+o=nt()
+        
