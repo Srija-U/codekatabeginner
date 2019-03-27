00ac9b65d058a750749f478c7f823381506af8d1
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-03-27 05:53:33 +0530

    Add files via upload

---
+++ checkk no within range.py
@@ -0,0 +1,8 @@
+class ncheck:
+    def __init__(self):
+        self.n=int(input())
+        if(self.n>=1)and(self.n<=10):
+            print("yes")
+        else:
+            print("no")
+o=ncheck()
