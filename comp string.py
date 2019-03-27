cca639041e294eefc3495122c9cc4b25c8098b5a
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-03-27 05:45:37 +0530

    Add files via upload

---
+++ comp string.py
@@ -0,0 +1,10 @@
+class st:
+    def __init__(self):
+        self.lis=[str(i) for i in input().split()]
+        if(self.lis[0]>=self.lis[1]):
+            print(self.lis[0])
+        else:
+            print(self.lis[1])
+o=st()
+        
+    
